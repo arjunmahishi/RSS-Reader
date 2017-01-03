64b729f97862125893aa2dd46c253a7bc7ea19c0
arjunmahishi <arjun.mahishi@gmail.com>
2017-01-03 23:50:11 +0530

    Listing titles

--- reader.py
+++ reader.py
@@ -3,3 +3,7 @@ import feedparser
 RSS_feed_url = "http://teckguide.blogspot.com/feeds/posts/default?alt=rss"
 atom_feed_url = "http://teckguide.blogspot.com/feeds/posts/default"
 
+feed = feedparser.parse(atom_feed_url)
+
+for entry in feed['entries']:
+	print entry['title']
