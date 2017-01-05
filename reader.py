0ca084f80ee067a4071a54308547f1fa81f43562
arjunmahishi <arjun.mahishi@gmail.com>
2017-01-05 20:33:57 +0530

    Made a title post pair

--- reader.py
+++ reader.py
@@ -1,9 +1,17 @@
 import feedparser
+from bs4 import BeautifulSoup
 
 RSS_feed_url = "http://teckguide.blogspot.com/feeds/posts/default?alt=rss"
 atom_feed_url = "http://teckguide.blogspot.com/feeds/posts/default"
 
 feed = feedparser.parse(atom_feed_url)
 
+post_and_title_pair = {}
+
 for entry in feed['entries']:
-	print entry['title']
+	postData = BeautifulSoup(entry['summary'], 'html.parser').text
+	post_and_title_pair[entry['title']] = postData
+	# print postData
+	# print
+
+print post_and_title_pair.keys()
